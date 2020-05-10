6c58b4344690bbe75c25615a1a33015202e5fa22
Victoria Stanko <vstanko1998@gmail.com>
2020-05-10 22:38:43 +0300

    Add graphql libraries to the project

---
+++ backend/routes/graphql.js
@@ -0,0 +1,36 @@
+// var express = require('express');
+// var graphqlHTTP = require('express-graphql');
+// var { buildSchema } = require('graphql');
+
+// // Construct a schema, using GraphQL schema language
+// var schema = buildSchema(`
+//   type Query {
+//     hello: String
+//   }
+// `);
+
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// module.exports = router;
+
+// var app = express();
+// app.use('/graphql', graphqlHTTP({
+//   schema: schema,
+//   rootValue: root,
+//   graphiql: true,
+// }));
+
+
+// TODO read after initail node js introduction
+// https://habr.com/ru/post/424199/
+// https://github.com/graphql/express-graphql
+
+
+// tools for angular 
+// https://www.howtographql.com/angular-apollo/1-getting-started/
